fe2756e456024b60d160eee19f862bfcd7fb2190
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-08 00:22:42 +0100

    feat: screen contatiner component

---
+++ src/component/container.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export const Container = ({
+  children,
+  barColor = "white",
+  backgroundColor,
+}) => {
+  return (
+    <>
+      <StatusBar style={"dark"} />
+      <SafeAreaView style={{ backgroundColor: barColor }} />
+      <View style={{ flex: 1, backgroundColor }}>{children}</View>
+    </>
+  );
+};
